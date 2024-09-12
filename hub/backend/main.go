d4f1867fbde7c5b084eb3bba83765399305255ef
GrimalDev <grimaldev@grimaldev.local>
2024-09-12 15:57:35 +0200

    tst

--- hub/backend/main.go
+++ hub/backend/main.go
@@ -12,15 +12,16 @@ func sensorInit(c echo.Context) error {
 	websocket.Handler(func(ws *websocket.Conn) {
 		defer ws.Close()
 		for {
-			// Write
-			err := websocket.Message.Send(ws, "Hello, Client!")
-			if err != nil {
-				c.Logger().Error(err)
-			}
+			// // Write
+			// fogger := `{"fogger": 90}`
+			// err := websocket.Message.Send(ws, fogger)
+			// if err != nil {
+			// 	c.Logger().Error(err)
+			// }
 
 			// Read
 			msg := ""
-			err = websocket.Message.Receive(ws, &msg)
+			err := websocket.Message.Receive(ws, &msg)
 			if err != nil {
 				c.Logger().Error(err)
 			}
